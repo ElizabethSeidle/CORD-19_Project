cf39757aff59449e274b68b04e7b642ca7ba64a0
Elizabeth Rockey Seidle <59021292+ElizabethSeidle@users.noreply.github.com>
2020-10-29 13:54:56 -0400

    Add files via upload

---
+++ Code/TEMPLATE_CODE_add_field_to_collection.py
@@ -0,0 +1,21 @@
+#adding new field to 'preprocess' collection -- NOTE: took me ~1hr to run
+
+
+#pd_df = your pandas dataframe
+#pd_df_variable = your column/variable from pandas dataframe that you want to copy into the collection
+#newField = new field being created in MongoDB collection
+
+
+client = pymongo.MongoClient("mongodb+srv://group3:group3psu!@squid.36jsw.mongodb.net/CORD19?retryWrites=true&w=majority")
+db = client.CORD19
+collection = db.preprocess
+cursor = collection.find()
+ 
+for document in cursor:
+    id = document["_id"]
+    record = pd_df.loc[(pd_df['_id'] == id)]
+    pd_df_variable = record["pd_df_variable"]
+    newField = document.get("newField")
+ 
+    if newField is None:
+        collection_clean.update_one({"_id": id}, {"$set": {"newField": pd_df_variable.tolist()}})
